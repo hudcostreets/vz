98d0750f6e6987316138013bbdc533320b65de9f
Ryan Williams <ryanw@hudcostreets.org>
2025-03-29 13:49:45 -0400

    footer bg

--- slide-top.vue
+++ slide-top.vue
@@ -2,7 +2,7 @@
   <footer class="absolute bottom-0 left-0 right-0 p-2">
     <div class="absolute bottom-0 right-0 footer flex flex-col items-center">
       <a target="_blank" class="foot-url" href="https://bw25.hccs.dev">bw25.hccs.dev</a>
-      <a target="_blank" href="https://hudcostreets.org"><img src="/HCCS_ID_Knockout_Hi.png" /></a>
+      <a target="_blank" href="https://hudcostreets.org"><div class="foot-logo"/></a>
     </div>
   </footer>
 </template>
